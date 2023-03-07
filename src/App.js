e06b403e5695bf70d7a2ba02a49c0bf55d084f3b
Akira Murakami <81897087+Set022525@users.noreply.github.com>
2023-03-07 22:05:40 +0900

    add wavesurfer.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import './App.css';
 import WaveSurfer from 'wavesurfer.js';
 import { useRef, useState } from 'react';
-// import { render } from "react-dom";
-// import { ReactDOM } from 'react';
+// import { analyze } from "web-audio-beat-detector";
 
 const App = () => {
   var context = null;
   const waveformRef = useRef(null);
-  const [playing, setPlaying] = useState(false);
-
   const waveformRef1 = useRef(null);
   const waveformRef2 = useRef(null);
   const waveformRef3 = useRef(null);
+  // const [array, setArray] = useState([]);
   const [array1, setArray1] = useState([]);
   const [array2, setArray2] = useState([]);
   const [array3, setArray3] = useState([]);
 
-  const handlePlayPause = () => {
-    setPlaying(!playing)
-    waveformRef.current.playPause();
-    console.log("handlePlayPause")
-  }
-  
-  const handleChangeFile = (e) => {
+  const [playing, setPlaying] = useState(false);
+
+
+  // const bpm = useRef();
+
+  const handleChangeFile = async (e) => {
     if (context == null) {
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       context = new AudioContext();
@@ -32,32 +29,72 @@ const App = () => {
          audioContext: context,
       });
     }
+
     const file = e.target.files[0]
     if (file) {
-        const fileUrl = URL.createObjectURL(file)
+      // try {
+      //   // also create webaudio from array buffer (not needed here)
+      //   const arrayBuffer = await file.arrayBuffer();
+      //   const audioBuffer = await context.decodeAudioData(arrayBuffer);
+      //   bpm.current = await analyze(audioBuffer);
+      // } catch (e) {
+      //   // console.log(e.message);
+      //   if (
+      //     e.message ===
+      //     "The given channelData does not contain any detectable beats."
+      //   ) {
+      //     console.log(e.message);
+      //     bpm.current = "-";
+      //   }
+      // }
 
-        //show the raw audio waveform
-        waveformRef.current.load(fileUrl);
+      // try {
+      //   // get peaks (must use a new, undetached array buffer from same file)
+      //   const newArrBuff = await file.arrayBuffer();
+      //   let blob = new Blob([new Uint8Array(newArrBuff)], { type: file.type });
+      //   console.log(blob);
 
-        //set the calculated value to array1-3
-        setArray1(waveletTransform());
-        setArray2(waveletTransform());
-        setArray3(waveletTransform());
+      //   // load url, filepath or blob
+      //   // ws.current.load("/out.mp3");
+      //   waveformRef.current.loadBlob(blob);
+      // } catch (e) {
+      //   console.log(e);
+      // }
     
-        waveformRef1.current = WaveSurfer.create({
-          container: waveformRef1.current,
-        });
-        waveformRef2.current = WaveSurfer.create({
-          container: waveformRef2.current,
-        });
-        waveformRef3.current = WaveSurfer.create({
-          container: waveformRef3.current,
-        });
+  // //
+  //       const fileReader = new FileReader()
+  //       fileReader.onload = () => {
+  //         // fileReader.resultにデータが入る
+  //         console.log(fileReader.result);
+  //       }
+  //       //set the raw value to array
+  //       setArray(fileReader.readAsArrayBuffer(file));
+      
+  //       //show the raw audio waveform
+  //       waveformRef.current.load({peaks: array});
+  // //
 
-        //show the result of wavelet transform
-        waveformRef1.current.load({peaks: array1});
-        waveformRef2.current.load({peaks: array2});
-        waveformRef3.current.load({peaks: array3});
+      const fileUrl = URL.createObjectURL(file);
+      waveformRef.current.load(fileUrl);
+
+      //set the calculated value to array1-3
+      setArray1(waveletTransform());
+      setArray2(waveletTransform());
+      setArray3(waveletTransform());
+    
+      //show the result of wavelet transform
+      waveformRef1.current = WaveSurfer.create({
+        container: waveformRef1.current,
+      });
+      waveformRef2.current = WaveSurfer.create({
+        container: waveformRef2.current,
+      });
+      waveformRef3.current = WaveSurfer.create({
+        container: waveformRef3.current,
+      });
+      waveformRef1.current.load({peaks: array1});
+      waveformRef2.current.load({peaks: array2});
+      waveformRef3.current.load({peaks: array3});
     }
   }
 
@@ -66,24 +103,11 @@ const App = () => {
     return [];
   }
 
-  // const startCalc = () => {
-  //   setArray1(waveletTransformation());
-  //   setArray2(waveletTransformation());
-  //   setArray3(waveletTransformation());
-
-  //   waveformRef1.current = WaveSurfer.create({
-  //     container: waveformRef1.current,
-  //   });
-  //   waveformRef2.current = WaveSurfer.create({
-  //     container: waveformRef2.current,
-  //   });
-  //   waveformRef3.current = WaveSurfer.create({
-  //     container: waveformRef3.current,
-  //   });
-  //   waveformRef1.current.load({peaks: array1});
-  //   waveformRef2.current.load({peaks: array2});
-  //   waveformRef3.current.load({peaks: array3});
-  // }
+  const handlePlayPause = () => {
+    setPlaying(!playing)
+    waveformRef.current.playPause();
+    console.log("handlePlayPause")
+  }
 
   return (
       <div className='App'>
@@ -91,7 +115,6 @@ const App = () => {
         <div ref={waveformRef} />
         <input type="file" accept='audio/*' onChange={(e) => handleChangeFile(e)}/>
         <button onClick={handlePlayPause}> {playing? "Pause" : "Play"} </button>
-        {/* <button onClick={startCalc}>calc</button> */}
 
         <p>n=1</p>
         <div ref={waveformRef1}/>
